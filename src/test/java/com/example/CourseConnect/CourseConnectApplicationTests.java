756498b04778bc8aa6fc4c038e3867550b1eaf84
Florin <f_ionut47@yahoo.com>
2024-10-04 18:20:21 +0300

    first commit

---
+++ src/test/java/com/example/CourseConnect/CourseConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
